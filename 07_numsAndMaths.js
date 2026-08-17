f04667220cd8bb41d2b6a4fcfc8e1cc98c6a58c9
taniyasharma608093-debug <taniyasharma608093@gmail.com>
2026-08-17 14:38:27 +0000

    nums n math

---
+++ 07_numsAndMaths.js
@@ -0,0 +1,32 @@
+const score = 400
+console.log(score)
+
+const balance = new Number(100)
+console.log(balance)
+
+console.log(balance.toString().length)
+console.log(balance.toFixed(2));
+
+const otherNumber = 123.89366
+console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000;
+console.log(hundreds.toLocaleString('en-IN'));
+
+//********************* Maths********************
+
+console.log(Math);
+console.log(Math.abs(-4));   //absolute
+console.log(Math.round(4.6));  //round off
+console.log(Math.ceil(4.6));  //round off to upper 
+console.log(Math.floor(4.6));  //round off to lower
+console.log(Math.min(4, 5, 6, 7));  //min in an array
+console.log(Math.max(4, 5, 6, 7));  //max in an array
+
+console.log(Math.random());  //0 to 1
+console.log((Math.random() * 10) + 1);  //1 to 9
+console.log(Math.floor((Math.random() * 10) + 1));
+
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
