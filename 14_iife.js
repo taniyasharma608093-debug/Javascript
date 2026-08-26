c3c6c26ea639faa20330dd149f34da99512066c1
taniyasharma608093-debug <taniyasharma608093@gmail.com>
2026-08-26 17:17:55 +0000

    iife

---
+++ 14_iife.js
@@ -0,0 +1,11 @@
+//Immediately Invoked Function Expression (IIFE)
+
+(function chai() {                  //global scope k pollution se bchane k liye 
+    console.log(`DB CONNECTED`);
+})();                               //without semicolon , next function will generate error
+
+// (function definition)(execution call)
+
+((name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+})('taniya');
